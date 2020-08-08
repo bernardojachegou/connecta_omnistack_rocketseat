087fb1e42ed12bc0f3d1ba29921f512a4ed28153
Michel Bernardo <bernardoenglish@outlook.com>
2020-08-08 03:42:28 -0400

    web and api done!

--- web/src/components/PageHeader/TeacherItem/index.tsx
+++ web/src/components/PageHeader/TeacherItem/index.tsx
@@ -2,32 +2,57 @@ import React from 'react';
 
 import whatsappIcon from '../../../assets/images/icons/whatsapp.svg'
 import './styles.css'
+import api from '../../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+};
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/47263354?s=460&u=e02a441acc959b16334d30ab8e9d96f1adcb6b76&v=4" alt="Michel Bernardo" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Michel Bernardo</strong>
-                    <span>Inglês</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Professor de Inglês com experência em ensino de língua inglesa para fins específicos.
-            <br /><br />
-            Especialista em ambientes digitais e amante de tecnologias.
-        </p>
+                {teacher.bio}
+            </p>
 
             <footer>
                 <p>Preço/hora
-            <strong>R$ 50,00 </strong>
+            <strong>R$ {teacher.cost} </strong>
                 </p>
-                <button type="button">
+                <a
+                    onClick={createNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}?text=Eu%20preciso%20de%20aulas`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <img src={whatsappIcon} alt="Whatsapp" />
                 Entrar em contato
-            </button>
+            </a>
             </footer>
         </article>
     );
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
 import './styles.css';
-import TeacherItem from '../../components/PageHeader/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/PageHeader/TeacherItem';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import api from '../../services/api';
+
 
 
 function TeacherList() {
+    const [teachers, setTeachers] = useState([]);
+
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeek_day] = useState('');
+    const [time, setTime] = useState('');
+
+    async function searchTeachers(e: FormEvent) {
+        e.preventDefault();
+
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time,
+            }
+        });
+
+        console.log(response.data);
+        setTeachers(response.data);
+    }
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
-                <form id="search-teachers">
+                <form id="search-teachers" onSubmit={searchTeachers}>
                     <Select
                         name="subject"
                         label="Matéria"
+                        value={subject}
+                        onChange={(e) => { setSubject(e.target.value) }}
                         options={[
                             { value: 'Artes', label: 'Artes' },
                             { value: 'Educação Física', label: 'Educação Física' },
@@ -35,6 +59,8 @@ function TeacherList() {
                     <Select
                         name="week_day"
                         label="Dia da semana"
+                        value={week_day}
+                        onChange={(e) => { setWeek_day(e.target.value) }}
                         options={[
                             { value: '0', label: 'Segunda' },
                             { value: '1', label: 'Terça' },
@@ -46,15 +72,24 @@ function TeacherList() {
 
                         ]}
                     />
-                    <Input type="time" name="time" label="Hora" />
+                    <Input
+                        type="time"
+                        name="time"
+                        label="Hora"
+                        value={time}
+                        onChange={(e) => { setTime(e.target.value) }}
+                    />
+
+                    <button type="submit">
+                        Buscar
+                    </button>
                 </form>
             </PageHeader>
 
             <main>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {teachers.map((teacher: Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher} />;
+                })}
             </main>
         </div>
     );
